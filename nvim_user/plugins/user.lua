87fa20c6a7bbb7a7c3dd5e33e54f48ebdfda6e64
Eros Rolfe <erosrolf@un-p2.kzn.21-school.ru>
2023-09-01 14:06:03 +0300

    add astronvim config

---
+++ nvim_user/plugins/user.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    -- build = "cd app && yarn install",
+    -- build = ":call mkdp#util#install()",
+    run = ":call mkdp#util#install()",
+  },
+}
+
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
